f9ebd86441865db2498ac388581f37910d0da715
Abhirup Kamath <aktechniqued@gmail.com>
2020-08-04 13:08:16 +0530

    train and eval code completed

---
+++ config.py
@@ -0,0 +1,14 @@
+DATA_DIR = r"F:\Projects\PersonalProjects-GitHub\facespoof-detection\data"
+TRAIN_FILE = r"F:\Projects\PersonalProjects-GitHub\facespoof-detection\data\train_folds.csv" 
+TEST_FILE = r"F:\Projects\PersonalProjects-GitHub\facespoof-detection\data\val_rgb.csv"
+BATCH_SIZE = 8
+EVAL_BATCH_SIZE = 32
+IMAGE_WIDTH = 224
+IMAGE_HEIGHT = 224
+NUM_WORKERS = 4
+EPOCHS = 1
+DEVICE = "cuda"
+LR = 1e-4
+WEIGHT_DECAY = 1e-5
+PIN_MEMORY = True
+
